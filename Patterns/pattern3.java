a00d60e9554fd8741cbcfb0b331743ab1e9070db
Karan <karanbharadwaj394@gmail.com>
2025-09-11 20:39:11 +0530

    11-09-25

---
+++ Patterns/pattern3.java
@@ -0,0 +1,30 @@
+/* Input Format: N = 3
+Result: 
+1
+1 2 
+1 2 3
+
+Input Format: N = 6
+Result:
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+ */
+
+import java.util.Scanner;
+
+public class pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/pattern7.java
@@ -0,0 +1,35 @@
+
+/* Input Format: N = 3
+Result: 
+ *  
+*** 
+*****   
+Input Format: N = 6
+Result:
+    *     
+   ***    
+  *****   
+ *******  
+********* 
+***********
+
+*/
+import java.util.*;;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
